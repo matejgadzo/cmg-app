9cfddd388203981e4f7b5f5414d958d3509b1dcb
gadmat <matej.gadzo5@gmail.com>
2021-08-26 22:58:45 +0200

    final with mobile layout

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,40 +1,45 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import data from '../../assets/data.json';
-
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  styleUrls: ['./gallery.component.css'],
 })
-export class GalleryComponent{
-
-  public parsedData:{id:number,value:string}[] = data;
+export class GalleryComponent {
+  public parsedData: { id: number; value: string }[] = data;
 
   public alertText = this.parsedData[1].value;
 
-  slideConfigMain = {"slidesToShow": 1, "slidesToScroll": 1, "arrows": false, "asNavFor": '.slider-nav', "infinite": false};
-  slideConfig = {"slidesToShow": 4, "slidesToScroll": 4, "asNavFor": '.slider-for', "focusOnSelect": true,  "infinite": false};
+  slideConfigMain = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.slider-nav',
+    infinite: false,
+  };
+  slideConfig = {
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.slider-for',
+    focusOnSelect: true,
+    infinite: false,
+  };
 
-  selectedSize = "14 ct";
+  selectedSize = '14 ct';
 
   public options = [
-    {id:1, name:"14 ct"},
-    {id:2, name:"28 ct"}
+    { id: 1, name: '14 ct' },
+    { id: 2, name: '28 ct' },
   ];
 
+  constructor() {}
 
-  constructor(){}
-
-  onChange(changed: any){
+  onChange(changed: any) {
     this.selectedSize = changed.name;
-    console.log(changed.name);
   }
 
-
-  buyNowAlert(event){
+  buyNowAlert() {
     alert(this.alertText);
-
   }
-
 }
